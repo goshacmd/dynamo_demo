94e89aa8c6caa99b92bb3492e2604dd9ea019cef
Gosha Arinich <me@goshakkk.name>
2012-08-22 19:30:00 +0300

    using PORT env var

--- lib/dynamo_demo.ex
+++ lib/dynamo_demo.ex
@@ -1,6 +1,6 @@
 defmodule DynamoDemo do
   def start do
     :ok = :application.start(:dynamo_demo)
-    Dynamo.Cowboy.run DynamoDemo.App
+    Dynamo.Cowboy.run DynamoDemo.App, port: System.get_env("PORT") || 3000
   end
 end
