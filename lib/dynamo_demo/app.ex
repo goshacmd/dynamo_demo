3719a6f953a665a8a9669049767bda16f8f0766b
Gosha Arinich <me@goshakkk.name>
2012-08-24 01:39:15 +0300

    exmaple of prepare/1 callback, returning json by default

--- lib/dynamo_demo/app.ex
+++ lib/dynamo_demo/app.ex
@@ -2,15 +2,21 @@ defmodule DynamoDemo.App do
   use Dynamo.Router
   use Dynamo.App
 
+  # Example of `prepare/1` callback.
+  prepare do
+    conn = conn.set_resp_header "X-Powered-By", "Dynamo"
+    conn = conn.set_resp_header "Content-Type", "application/json"
+  end
+
   get "/" do
-    conn.resp 200, "-----> It works.\n"
+    conn.resp 200, "{}\n"
   end
 
   get "/version" do
-    conn.resp 200, "#{DynamoDemo.version}\n"
+    conn.resp 200, "{\"version\":\"#{DynamoDemo.version}\"}\n"
   end
 
   get "/hello" do
-    conn.resp 200, "Hello World!\n"
+    conn.resp 200, "{}\n"
   end
 end
